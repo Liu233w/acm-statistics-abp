0cf8867669a3e68abfaf1b9d67e6e4bfeb322dc6
Liu's DeskTop <wwwlsmcom@msn.cn>
2018-04-05 21:49:24 +0800

    feat(*): 重新生成数据库 Migration

--- src/AcmStatisticsAbp.EntityFrameworkCore/Migrations/AcmStatisticsAbpDbContextModelSnapshot.cs
+++ src/AcmStatisticsAbp.EntityFrameworkCore/Migrations/AcmStatisticsAbpDbContextModelSnapshot.cs
@@ -22,8 +22,8 @@ namespace AcmStatisticsAbp.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.0.1-rtm-125")
-                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+                .HasAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                .HasAnnotation("ProductVersion", "2.0.1-rtm-125");
 
             modelBuilder.Entity("Abp.Application.Editions.Edition", b =>
                 {
