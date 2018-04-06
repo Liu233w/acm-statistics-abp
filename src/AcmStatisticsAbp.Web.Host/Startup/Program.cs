026be550a7ac64d26e0f8155bf8afd78e34f0d1e
Liu's DeskTop <wwwlsmcom@msn.cn>
2018-04-06 15:58:39 +0800

    feat(*): 更改端口

--- src/AcmStatisticsAbp.Web.Host/Startup/Program.cs
+++ src/AcmStatisticsAbp.Web.Host/Startup/Program.cs
@@ -4,8 +4,10 @@
 
 namespace AcmStatisticsAbp.Web.Host.Startup
 {
+    using System.IO;
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
 
     public class Program
     {
@@ -16,8 +18,15 @@ namespace AcmStatisticsAbp.Web.Host.Startup
 
         public static IWebHost BuildWebHost(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hosting.json", optional: true)
+                .AddCommandLine(args)
+                .Build();
+
             return WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseConfiguration(config)
                 .Build();
         }
     }
