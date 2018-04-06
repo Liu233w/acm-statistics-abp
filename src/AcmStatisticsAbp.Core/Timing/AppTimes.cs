90ba9de3d7eaa4773744b1ec038cb04b0f5ff385
Liu's DeskTop <wwwlsmcom@msn.cn>
2018-04-06 14:56:13 +0800

    docs(*): fix SA1623

--- src/AcmStatisticsAbp.Core/Timing/AppTimes.cs
+++ src/AcmStatisticsAbp.Core/Timing/AppTimes.cs
@@ -10,7 +10,7 @@ namespace AcmStatisticsAbp.Timing
     public class AppTimes : ISingletonDependency
     {
         /// <summary>
-        /// Gets the startup time of the application.
+        /// Gets or sets the startup time of the application.
         /// </summary>
         public DateTime StartupTime { get; set; }
     }
