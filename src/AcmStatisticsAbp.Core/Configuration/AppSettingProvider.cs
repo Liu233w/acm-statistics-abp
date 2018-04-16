a4042671ebb54fbfd5943b655b9b44533aba7628
Liu's DeskTop <wwwlsmcom@msn.cn>
2018-04-16 18:10:38 +0800

    feat(后端): 在设置中开启邮件验证

--- src/AcmStatisticsAbp.Core/Configuration/AppSettingProvider.cs
+++ src/AcmStatisticsAbp.Core/Configuration/AppSettingProvider.cs
@@ -6,11 +6,18 @@ namespace AcmStatisticsAbp.Configuration
 {
     using System.Collections.Generic;
     using Abp.Configuration;
+    using Abp.Zero.Configuration;
 
     public class AppSettingProvider : SettingProvider
     {
         public override IEnumerable<SettingDefinition> GetSettingDefinitions(SettingDefinitionProviderContext context)
         {
+            // 开启邮件验证
+            var emailConfirmationSetting =
+                context.Manager.GetSettingDefinition(
+                    AbpZeroSettingNames.UserManagement.IsEmailConfirmationRequiredForLogin);
+            emailConfirmationSetting.DefaultValue = "true";
+
             return new[]
             {
                 new SettingDefinition(AppSettingNames.UiTheme, "red", scopes: SettingScopes.Application | SettingScopes.Tenant | SettingScopes.User, isVisibleToClients: true),
