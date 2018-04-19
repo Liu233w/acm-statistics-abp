e3eb9b07514e8720fec5a93d21d4c4ceb96f2555
Liu's DeskTop <wwwlsmcom@msn.cn>
2018-04-19 21:56:11 +0800

    feat(后端-SettingProvider): 将说明放进 Description 字段中，令其可查看查看

--- src/AcmStatisticsAbp.Core/Configuration/AppSettingProvider.cs
+++ src/AcmStatisticsAbp.Core/Configuration/AppSettingProvider.cs
@@ -6,6 +6,7 @@ namespace AcmStatisticsAbp.Configuration
 {
     using System.Collections.Generic;
     using Abp.Configuration;
+    using Abp.Localization;
 
     public class AppSettingProvider : SettingProvider
     {
@@ -15,11 +16,15 @@ namespace AcmStatisticsAbp.Configuration
             {
                 new SettingDefinition(AppSettingNames.UiTheme, "red", scopes: SettingScopes.Application | SettingScopes.Tenant | SettingScopes.User, isVisibleToClients: true),
 
-                // 默认发送邮件的域名和协议，结尾不能加上斜杠
-                new SettingDefinition(AppSettingNames.EmailConfirmationBaseUrl, "http://new.npuacm.info"),
+                new SettingDefinition(
+                    AppSettingNames.EmailConfirmationBaseUrl,
+                    "http://new.npuacm.info",
+                    description: new FixedLocalizableString("默认发送邮件的域名和协议，结尾不能加上斜杠")),
 
-                // 发送确认邮件的最短间隔，用秒来表示（低于此间隔将不能发送确认邮件，防止发送过于频繁）
-                new SettingDefinition(AppSettingNames.MinEmailConfirmationSendInterval, "60"),
+                new SettingDefinition(
+                    AppSettingNames.MinEmailConfirmationSendInterval,
+                    "60",
+                    description: new FixedLocalizableString("发送确认邮件的最短间隔，用秒来表示（低于此间隔将不能发送确认邮件，防止发送过于频繁）")),
             };
         }
     }
