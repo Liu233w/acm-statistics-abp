957258612feab582e45030ecf558625995ae8ee9
Liu233w <wwwlsmcom@outlook.com>
2018-04-19 11:46:01 +0800

    test(后端-邮件验证): 补充测试用例，提高覆盖率

---
+++ test/AcmStatisticsAbp.Tests/EmailConfirmation/EmailConfirmationManager_Test.cs
@@ -0,0 +1,120 @@
+// <copyright file="EmailConfirmationManager_Test.cs" company="西北工业大学ACM开发组">
+// Copyright (c) 西北工业大学ACM开发组. All rights reserved.
+// </copyright>
+
+namespace AcmStatisticsAbp.Tests.EmailConfirmation
+{
+    using System;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Abp.Configuration;
+    using Abp.Net.Mail;
+    using Abp.Net.Mail.Smtp;
+    using Abp.UI;
+    using AcmStatisticsAbp.Authorization.EmailConfirmation;
+    using AcmStatisticsAbp.Authorization.Users;
+    using Bogus;
+    using Microsoft.EntityFrameworkCore;
+    using Shouldly;
+    using Xunit;
+
+    public class EmailConfirmationManager_Test : AcmStatisticsAbpTestBase
+    {
+        private readonly EmailConfirmationManager emailConfirmationManager;
+        private readonly UserRegistrationManager userRegistrationManager;
+
+        private class RegisterUserParam
+        {
+            public string Name { get; set; }
+
+            public string SurName { get; set; }
+
+            public string Email { get; set; }
+
+            public string Username { get; set; }
+
+            public string Password { get; set; }
+        }
+
+        private Faker<RegisterUserParam> registerUserParamFaker;
+
+        public EmailConfirmationManager_Test()
+        {
+            this.emailConfirmationManager = this.Resolve<EmailConfirmationManager>(new
+            {
+                emailSender = this.Resolve<NullEmailSender>(),
+            });
+
+            this.userRegistrationManager = this.Resolve<UserRegistrationManager>();
+
+            this.registerUserParamFaker = new Faker<RegisterUserParam>()
+                    .StrictMode(true)
+                    .RuleFor(e => e.Name, f => f.Name.FirstName())
+                    .RuleFor(e => e.SurName, f => f.Name.LastName())
+                    .RuleFor(e => e.Username, f => f.Internet.UserName())
+                    .RuleFor(e => e.Email, f => f.Internet.Email())
+                    .RuleFor(e => e.Password, f => f.Internet.Password())
+                ;
+        }
+
+        private async Task<User> BuildTestUser()
+        {
+            var user = this.registerUserParamFaker.Generate();
+
+            User ret = null;
+            await this.WithUnitOfWorkAsync(async () =>
+            {
+                ret = await this.userRegistrationManager.RegisterAsync(
+                    user.Name,
+                    user.SurName,
+                    user.Email,
+                    user.Username,
+                    user.Password,
+                    false);
+            });
+
+            return ret;
+        }
+
+        [Fact]
+        public async Task SendConfirmationEmailAsync_应该生成带有正确用户ID的记录()
+        {
+            // Arrange
+            var user1 = await this.BuildTestUser();
+            var user2 = await this.BuildTestUser();
+
+            Debug.Assert(user1.Id != user2.Id, "确保已经生成了用户ID并且ID有效");
+
+            // Act
+            await this.WithUnitOfWorkAsync(async () =>
+            {
+                await this.emailConfirmationManager.SendConfirmationEmailAsync(user1);
+                await this.emailConfirmationManager.SendConfirmationEmailAsync(user2);
+            });
+
+            // Assert
+            await this.UsingDbContextAsync(async ctx =>
+            {
+                var confirmationCodes = await ctx.ConfirmationCodes.ToListAsync();
+
+                confirmationCodes.Count.ShouldBe(2);
+                confirmationCodes[0].Id.ShouldNotBe(confirmationCodes[1].Id);
+                confirmationCodes.Select(item => item.UserId).ShouldBe(new[] { user1.Id, user2.Id });
+            });
+        }
+
+        [Fact]
+        public async Task TryConfirmEmailAsync_在Token不存在或格式不正确时应当报错()
+        {
+            await this.WithUnitOfWorkAsync(async () =>
+            {
+                (await this.emailConfirmationManager.TryConfirmEmailAsync("000")
+                    .ShouldThrowAsync<UserFriendlyException>()).Code.ShouldBe(3);
+
+                (await this.emailConfirmationManager.TryConfirmEmailAsync(default(Guid).ToString())
+                    .ShouldThrowAsync<UserFriendlyException>()).Code.ShouldBe(3);
+            });
+        }
+    }
+}
