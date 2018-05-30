4e13b7954b3764b10fdd950f1fe1128f81c1a3ed
Liu's DeskTop <wwwlsmcom@msn.cn>
2018-05-30 19:46:44 +0800

    feat(WorkerUsernamesAppService): 完成权限控制

--- src/AcmStatisticsAbp.Application/SubmissionStatistics/WorkerUsernamesAppService.cs
+++ src/AcmStatisticsAbp.Application/SubmissionStatistics/WorkerUsernamesAppService.cs
@@ -4,16 +4,21 @@
 
 namespace AcmStatisticsAbp.SubmissionStatistics
 {
+    using System.Linq;
     using System.Threading.Tasks;
     using Abp.Application.Services;
+    using Abp.Application.Services.Dto;
     using Abp.Authorization;
     using Abp.Domain.Repositories;
+    using Abp.UI;
     using AcmStatisticsAbp.Authorization;
+    using AcmStatisticsAbp.Exceptions;
     using AcmStatisticsAbp.Helpers;
     using AcmStatisticsAbp.SubmissionStatistics.Dto;
 
     [AbpAuthorize(PermissionNames.Pages_WorkerUsername)]
-    public class WorkerUsernamesAppService : AsyncCrudAppService<WorkerUsernames, WorkerUsernamesDto, long>, IWorkerUsernamesAppService
+    public class WorkerUsernamesAppService : AsyncCrudAppService<WorkerUsernames, WorkerUsernamesDto, long,
+        PagedAndSortedResultRequestDto, WorkerUsernamesDto>, IWorkerUsernamesAppService
     {
         public WorkerUsernamesAppService(IRepository<WorkerUsernames, long> repository)
             : base(repository)
@@ -29,19 +34,49 @@ namespace AcmStatisticsAbp.SubmissionStatistics
 
         public override async Task<WorkerUsernamesDto> Update(WorkerUsernamesDto input)
         {
-            // ReSharper disable once PossibleInvalidOperationException
-            var userId = this.AbpSession.UserId.Value;
-
-            var entity = await this.Repository.FirstOrThrowAsync(input.Id);
-            if (entity.UserId != userId)
-            {
-                throw new AbpAuthorizationException("不能修改其他用户的数据");
-            }
+            var entity = await this.CheckOwnerAndGetEntity(input.Id);
 
             entity.Title = input.Title;
             entity.Usernames = input.Usernames;
 
             return this.MapToEntityDto(entity);
+        }
+
+        public override async Task Delete(EntityDto<long> input)
+        {
+            var entity = await this.CheckOwnerAndGetEntity(input.Id);
+            await this.Repository.DeleteAsync(entity);
+        }
+
+        public override async Task<WorkerUsernamesDto> Get(EntityDto<long> input)
+        {
+            var entity = await this.CheckOwnerAndGetEntity(input.Id);
+            return this.MapToEntityDto(entity);
+        }
+
+        protected override IQueryable<WorkerUsernames> CreateFilteredQuery(PagedAndSortedResultRequestDto input)
+        {
+            // 覆写这个方法就可以给 GetAll 添加权限控制了
+            return this.Repository.GetAll().Where(item => item.UserId == this.AbpSession.UserId.Value);
+        }
+
+        /// <summary>
+        /// 尝试获取实体。如果此实体不属于用户，将抛出异常。
+        /// </summary>
+        /// <param name="id"></param>
+        private async Task<WorkerUsernames> CheckOwnerAndGetEntity(long id)
+        {
+            // ReSharper disable once PossibleInvalidOperationException
+            var userId = this.AbpSession.UserId.Value;
+
+            var entity = await this.Repository.FirstOrThrowAsync(id);
+            if (entity.UserId != userId)
+            {
+                var exception = new UserFriendlyException(StaticErrorCode.EntityNotFound, "未找到此ID");
+                throw exception;
+            }
+
+            return entity;
         }
     }
 }
